5f3873e2d523ddc21ae8480dbdc48c1b882372f8
Jack Urquizu <42326640+jackstruction@users.noreply.github.com>
2022-02-05 16:47:26 -0600

    Compiled
    This works at a minimum amount.

--- TokenComrade.sol
+++ TokenComrade.sol
@@ -1,9 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+
 contract TokenComrade{
+
+    // GLobal Variables
     address payable owner;
+    uint numEvents;
+    bool fundPulled;
+
+    // declares a state variable that stores a `Holder` struct for each possible address.
+
+    struct Holder {
+        address wallet;
+        uint inventory;
+    }
+    
     // The events that admission tokens are created for
-    struct Event {
+    
+    struct Event{
         address payable organizer;
         string name;
         string description;
@@ -11,59 +25,76 @@ contract TokenComrade{
         uint supply;
         uint price; //wei
         bool soldOut;
+        uint256 redeemableBalance;
+        mapping(address => bool) scanners; // stores boolean to check whether an address is an approved scanner.
+        //maps address to uint to store the quantity of owned tickets
+        mapping(address => uint) inventory;
     }
 
-    struct Holder {
-        address wallet;
-        uint inventory;
-
-    }
-
-    uint256 public redeemableBalance;
-    address[] public approvedScanners;  
-    constructor(){
-        owner = msg.sender;
-    }
-    uint numEvents;
+    mapping(address => Holder) holders;
     mapping (uint => Event) events;
-    //Owner Section | Create tickets, Pull funds, Assign scanners
-    function createEvent(uint _supply, bytes32 _name, uint _price) external payable public {
-    eventID = numEvents++;
-    Event storage e = events[eventID];
-    e.initialSupply = _supply;
-    e.supply = _supply;
-    e.name = _name;
-    e.price = _price;
-    e.organizer = msg.sender;
-    e.soldOut = false;
-    }
-    function pullFunds(uint eventID) external public{
-        require (msg.sender == owner, "Ah ah ah, that money's for someone else")
-        owner.transfer(uint256 redeemableBalance);
-    }
-    function approveScanner(address _scanner, uint eventID) public external (returns bool){
-        Event storage e = events[eventID];
-        for(uint i=0;i<e.approvedScanners.length;i++){
-            if(e.approvedScanner[i]==_scanner){
-                uint one = 1;
-            } else {approvedScanners.push(_scanner)}
-        }
-    }
-    function checkSoldOut(uint eventID) view{
-        
-    }
 
-    function mintTicket(uint eventID, uint _quantity) public payable{
+    //alls I know is that I'm the owner.
+    constructor(){
+        owner = payable(msg.sender);
+    }
+    
+    //Owner Section | Create tickets, Pull funds, Assign scanners
+    function createEvent(uint _supply, string memory _name, uint _price) external payable returns (uint, uint, uint){
+        uint eventID = numEvents++;
+        Event storage e = events[eventID];
+        e.initialSupply = _supply;
+        e.supply = _supply;
+        e.name = _name;
+        e.price = _price;
+        e.organizer = payable(msg.sender);
+        e.soldOut = false;
+        e.redeemableBalance = 0;
+        return (eventID, e.initialSupply, e.price);
+    }
+    
+    function mint(uint eventID, uint _quantity) external payable{
         Event storage e = events[eventID];
         require(msg.value >= e.price, "incorrect value");
+        require(e.supply >= _quantity, "Not Enough Supply");
+        address holderID = msg.sender;
+        Holder storage h = holders[holderID];
         e.supply -= _quantity;
-        buyer.ticketsHeld += quantity
-    }
-    function scanTicket(address _entrant, uint _quantity)public external{
-        Event storage e = events[eventID];
-        require(msg.sender == e.approvedScanner, "Not an approved scanner");
-        if(_entrant in e.Holders[]){
-            _entrant.
+        h.inventory += _quantity;
+        h.wallet = msg.sender;
+        if(e.supply == 0){
+            sellOut(eventID);
         }
     }
+
+    function scan(uint eventID, address _holderID, uint _quantity) external {
+        Event storage e = events[eventID];
+        Holder storage h = holders[_holderID];
+        require(e.scanners[msg.sender], "You are not an approved scanner");
+        require(h.inventory > _quantity, "Not enough tokens");
+        h.inventory -= _quantity;
+        e.redeemableBalance += (_quantity * e.price);
+    }
+
+    function approveScanner(address _scanner, uint eventID) public{
+        Event storage e = events[eventID];
+        require(e.organizer == msg.sender);
+        e.scanners[_scanner] = true;
+    }
+
+    function sellOut(uint eventID) internal {
+        Event storage e = events[eventID];
+        e.soldOut = true;
+    }
+
+    function pullFunds() external returns (bytes32){
+        require(msg.sender == owner, "Ah ah ah, that money's for someone else");
+        require(address(this).balance>0);
+        owner.transfer(address(this).balance);        
+        fundPulled = true;
+        bytes32 successmsg = "success";
+        return successmsg;
+        //change to just the extra from events that are over.
+    }
+
 }
